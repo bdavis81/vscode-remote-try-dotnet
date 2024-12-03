5570990e8d0529a57c04d7af36067fa08bf5bb0b
Brian Davis <152680410+bdavis81@users.noreply.github.com>
2024-12-03 18:06:56 +0000

    Config and update name

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var app = builder.Build();
 
 app.Urls.Add("http://localhost:5000");
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello, Brian!");
 
 app.MapGet("/{cityName}/weather", GetWeatherByCity);
 
